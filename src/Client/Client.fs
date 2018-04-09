9ecc8f49e9b8003ed70c802427665d1867302471
Jeremy Abbott <jeremymabbott@gmail.com>
2018-04-09 12:30:39 -0700

    Update home page

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -32,10 +32,10 @@ type Msg =
 let init () : Model * Cmd<Msg> =
   let model = None
   let cmd =
-    Cmd.ofPromise 
-      (fetchAs<int> "/api/init") 
-      [] 
-      (Ok >> Init) 
+    Cmd.ofPromise
+      (fetchAs<int> "/api/init")
+      []
+      (Ok >> Init)
       (Error >> Init)
   model, cmd
 
@@ -48,102 +48,79 @@ let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
     | _ -> None
   model', Cmd.none
 
-let safeComponents =
-  let intersperse sep ls =
-    List.foldBack (fun x -> function
-      | [] -> [x]
-      | xs -> x::sep::xs) ls []
-
-  let components =
-    [
-      "Saturn", "https://saturnframework.github.io/docs/"
-      "Fable", "http://fable.io"
-      "Elmish", "https://fable-elmish.github.io/"
-      "Fulma", "https://mangelmaxime.github.io/Fulma" 
-      "Bulma\u00A0Templates", "https://dansup.github.io/bulma-templates/"
-    ]
-    |> List.map (fun (desc,link) -> a [ Href link ] [ str desc ] )
-    |> intersperse (str ", ")
-    |> span [ ]
-
-  p [ ]
-    [ strong [] [ str "SAFE Template" ]
-      str " powered by: "
-      components ]
-
 let show = function
 | Some x -> string x
 | None -> "Loading..."
 
 let navBrand =
-  Navbar.Brand.div [ ] 
-    [ Navbar.Item.a 
+  Navbar.Brand.div [ ]
+    [ Navbar.Item.a
         [ Navbar.Item.Props [ Href "https://safe-stack.github.io/" ]
-          Navbar.Item.IsActive true ] 
+          Navbar.Item.IsActive true ]
         [ img [ Src "https://safe-stack.github.io/images/safe_top.png"
-                Alt "Logo" ] ] 
-      Navbar.burger [ ] 
+                Alt "Logo" ] ]
+      Navbar.burger [ ]
         [ span [ ] [ ]
           span [ ] [ ]
           span [ ] [ ] ] ]
 
 let navMenu =
   Navbar.menu [ ]
-    [ Navbar.End.div [ ] 
-        [ Navbar.Item.a [ ] 
-            [ str "Home" ] 
+    [ Navbar.End.div [ ]
+        [ Navbar.Item.a [ ]
+            [ str "Home" ]
           Navbar.Item.a [ ]
             [ str "Examples" ]
           Navbar.Item.a [ ]
             [ str "Documentation" ]
           Navbar.Item.div [ ]
-            [ Button.a 
+            [ Button.a
                 [ Button.Color IsWhite
                   Button.IsOutlined
                   Button.Size IsSmall
-                  Button.Props [ Href "https://github.com/SAFE-Stack/SAFE-template" ] ] 
-                [ Icon.faIcon [ ] 
+                  Button.Props [ Href "https://github.com/SAFE-Stack/SAFE-template" ] ]
+                [ Icon.faIcon [ ]
                     [ Fa.icon Fa.I.Github; Fa.fw ]
                   span [ ] [ str "View Source" ] ] ] ] ]
 
 let containerBox (model : Model) (dispatch : Msg -> unit) =
   Box.box' [ ]
-    [ Form.Field.div [ Form.Field.IsGrouped ] 
-        [ Form.Control.p [ Form.Control.CustomClass "is-expanded"] 
+    [ Form.Field.div [ Form.Field.IsGrouped ]
+        [ Form.Control.p [ Form.Control.CustomClass "is-expanded"]
             [ Form.Input.text
                 [ Form.Input.Disabled true
                   Form.Input.Value (show model) ] ]
           Form.Control.p [ ]
-            [ Button.a 
+            [ Button.a
                 [ Button.Color IsPrimary
                   Button.OnClick (fun _ -> dispatch Increment) ]
                 [ str "+" ] ]
           Form.Control.p [ ]
-            [ Button.a 
+            [ Button.a
                 [ Button.Color IsPrimary
                   Button.OnClick (fun _ -> dispatch Decrement) ]
                 [ str "-" ] ] ] ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
-  Hero.hero [ Hero.Color IsPrimary; Hero.IsFullHeight ] 
-    [ Hero.head [ ] 
+  Hero.hero [ Hero.Color IsPrimary; Hero.IsFullHeight ]
+    [ Hero.head [ ]
         [ Navbar.navbar [ ]
             [ Container.container [ ]
                 [ navBrand
                   navMenu ] ] ]
-      
-      Hero.body [ ] 
+
+      Hero.body [ ]
         [ Container.container [ Container.CustomClass Alignment.HasTextCentered ]
-            [ Column.column 
+            [ Column.column
                 [ Column.Width (Column.All, Column.Is6)
                   Column.Offset (Column.All, Column.Is3) ]
-                [ h1 [ ClassName "title" ] 
-                    [ str "SAFE Template" ]
+                [ h1 [ ClassName "title" ]
+                    [ str "Proxy Croak Codes" ]
                   div [ ClassName "subtitle" ]
-                    [ safeComponents ]
+                    [ str "Find the codes you need to create your deck in Proxy Croak" ]
                   containerBox model dispatch ] ] ] ]
 
-  
+
 #if DEBUG
 open Elmish.Debug
 open Elmish.HMR
