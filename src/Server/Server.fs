27c8362741ec237e0fb62ddd0f5d7a7abc1b6543
Jeremy Abbott <jeremymabbott@gmail.com>
2018-04-08 19:49:58 -0700

    Initial Commit

---
+++ src/Server/Server.fs
@@ -0,0 +1,48 @@
+﻿open System
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+
+open Giraffe
+
+open Giraffe.Serialization
+open Microsoft.Extensions.DependencyInjection
+
+open Shared
+
+let clientPath = Path.Combine("..","Client") |> Path.GetFullPath
+let port = 8085us
+
+let getInitCounter () : Task<Counter> = task { return 42 }
+
+let webApp : HttpHandler =
+  route "/api/init" >=>
+    fun next ctx ->
+      task {
+        let! counter = getInitCounter()
+        return! Successful.OK counter next ctx
+      }
+
+let configureApp  (app : IApplicationBuilder) =
+  app.UseStaticFiles()
+     .UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+    let fableJsonSettings = Newtonsoft.Json.JsonSerializerSettings()
+    fableJsonSettings.Converters.Add(Fable.JsonConverter())
+    services.AddSingleton<IJsonSerializer>(NewtonsoftJsonSerializer fableJsonSettings) |> ignore
+
+WebHost
+  .CreateDefaultBuilder()
+  .UseWebRoot(clientPath)
+  .UseContentRoot(clientPath)
+  .Configure(Action<IApplicationBuilder> configureApp)
+  .ConfigureServices(configureServices)
+  .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+  .Build()
+  .Run()
